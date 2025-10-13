5e8f4dc16b52c22c5fc0c5e2f7dddcc10ac5cd0e
nour ben hnia <nourbenhnia619@gmail.com>
2025-10-13 16:34:20 +0200

    used train_test_split and desciscion tree

--- project4/project4.py
+++ project4/project4.py
@@ -1,4 +1,7 @@
 import pandas as pd #to read the dataset
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 data=pd.read_csv("Iris.csv")
 #learning about head and tail
 print('first 5 rows')
@@ -9,3 +12,14 @@ print('last 5 rows')
 print(data.tail())
 print('last  2 rows')
 print(data.tail(2))
+#axis (1 =column, 0=row)
+#we droped species cuz theyre the label
+x=data.drop("Species", axis=1).values
+y=data["Species"].values
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=5)
+# random state :Python, shuffle my flowers but do it according to recipe #5
+model=DecisionTreeClassifier(random_state=5)
+model.fit(x_train,y_train)
+y_predict=model.predict(x_test)
+print("accuracy",accuracy_score(y_predict,y_test))
+
