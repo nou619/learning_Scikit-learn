99515de3993342c44561913502e18cc121b45751
nour ben hnia <nourbenhnia619@gmail.com>
2025-10-21 18:21:33 +0200

    tested on a new message

--- project9/project9.py
+++ project9/project9.py
@@ -13,4 +13,8 @@ x_train, x_test, y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=
 model=MultinomialNB()
 model.fit(x_train,y_train)
 y_pred=model.predict(x_test)
-print('accuracy',accuracy_score(y_test,y_pred))
+#print('accuracy',accuracy_score(y_test,y_pred))
+msg="Subject: Meeting Tomorrow at 10 AM | Body: Hi team, just a reminder we have a scheduled meeting tomorrow at 10 AM in the conference room. Please bring your updates."
+vec_msg=vectoriser.transform([msg])
+pred=model.predict(vec_msg)[0]
+print("Result:", "spam" if pred==1 else "ham")
