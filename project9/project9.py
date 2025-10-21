6d18ed69c0c05802ed496ab77cf7d0cc99cd3a3d
nour ben hnia <nourbenhnia619@gmail.com>
2025-10-21 18:08:25 +0200

    project 9

---
+++ project9/project9.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+df=pd.read_csv('spam.csv',encoding='latin-1')
+df=df[['v1','v2']]
+df.columns=['label','text']
+vectoriser=CountVectorizer()
+x=vectoriser.fit_transform(df['text'])
+y=df['label'].map({'ham':0,'spam':1})
+x_train, x_test, y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+model=MultinomialNB()
+model.fit(x_train,y_train)
+y_pred=model.predict(x_test)
+print('accuracy',accuracy_score(y_test,y_pred))
